594314b147ff397fb4853dc8b68b9e1e3b3fc7bb
Cedric Baaklini <cabaaklini@icloud.com>
2025-05-07 01:36:33 -0400

    Polishing a few things.

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GATConv
-import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pickle
